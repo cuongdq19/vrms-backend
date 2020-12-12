c81d8246aa3ff005b13afd4f72e0ab4e3cf97a1c
LordRose <lordrose1951980@gmail.com>
2020-12-12 21:27:25 +0700

    Added user data to DataLoader.

--- src/main/java/org/lordrose/vrms/bootstrap/DataLoader.java
+++ src/main/java/org/lordrose/vrms/bootstrap/DataLoader.java
@@ -3,16 +3,13 @@ package org.lordrose.vrms.bootstrap;
 import lombok.RequiredArgsConstructor;
 import org.lordrose.vrms.domains.Manufacturer;
 import org.lordrose.vrms.domains.Provider;
+import org.lordrose.vrms.domains.Role;
 import org.lordrose.vrms.domains.User;
 import org.lordrose.vrms.domains.Vehicle;
 import org.lordrose.vrms.domains.VehicleModel;
 import org.lordrose.vrms.repositories.ManufacturerRepository;
-import org.lordrose.vrms.repositories.ModelGroupRepository;
-import org.lordrose.vrms.repositories.PartCategoryRepository;
-import org.lordrose.vrms.repositories.PartSectionRepository;
 import org.lordrose.vrms.repositories.ProviderRepository;
-import org.lordrose.vrms.repositories.ServiceTypeDetailRepository;
-import org.lordrose.vrms.repositories.ServiceTypeRepository;
+import org.lordrose.vrms.repositories.RoleRepository;
 import org.lordrose.vrms.repositories.UserRepository;
 import org.lordrose.vrms.repositories.VehicleModelRepository;
 import org.lordrose.vrms.repositories.VehicleRepository;
@@ -31,11 +28,7 @@ public class DataLoader implements CommandLineRunner {
     private final ProviderRepository providerRepository;
     private final UserRepository userRepository;
     private final VehicleRepository vehicleRepository;
-    private final ServiceTypeRepository serviceTypeRepository;
-    private final ServiceTypeDetailRepository serviceTypeDetailRepository;
-    private final PartCategoryRepository partCategoryRepository;
-    private final ModelGroupRepository groupRepository;
-    private final PartSectionRepository sectionRepository;
+    private final RoleRepository roleRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -43,14 +36,28 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void loadAll() {
-        User owner_1 = userRepository.save(User.builder()
-                .phoneNumber("123456")
-                .password("1")
-                .fullName("Duong Huynh Huynh")
-                .gender(true)
-                .imageUrl("image url")
-                .isActive(true)
-                .deviceToken("device token")
+        Role admin = roleRepository.save(Role.builder()
+                .name("ADMIN")
+                .description("description")
+                .build());
+
+        Role user = roleRepository.save(Role.builder()
+                .name("USER")
+                .description("description")
+                .build());
+
+        Role manager = roleRepository.save(Role.builder()
+                .name("MANAGER")
+                .description("description")
+                .build());
+
+        Role staff = roleRepository.save(Role.builder()
+                .name("STAFF")
+                .description("description")
+                .build());
+        Role tech = roleRepository.save(Role.builder()
+                .name("TECHNICIAN")
+                .description("description")
                 .build());
         Manufacturer honda = manufacturerRepository.save(Manufacturer.builder()
                 .name("Honda")
@@ -86,6 +93,38 @@ public class DataLoader implements CommandLineRunner {
                 .slotCapacity(1)
                 .isActive(true)
                 .imageUrls("")
+                .build());
+
+        User admin_1 = userRepository.save(User.builder()
+                .isActive(true)
+                .username("admin")
+                .password("admin")
+                .fullName("Admin full name")
+                .imageUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgj76jmiFkhkGcQP9CffojdbmNLfZQjaGAhQ&usqp=CAU")
+                .gender(true)
+                .role(admin)
+                .build());
+
+        User owner_1 = userRepository.save(User.builder()
+                .phoneNumber("123456")
+                .password("1")
+                .fullName("Duong Huynh Huynh")
+                .gender(true)
+                .imageUrl("image url")
+                .isActive(true)
+                .deviceToken("device token")
+                .role(user)
+                .build());
+
+        User mn1 = userRepository.save(User.builder()
+                .isActive(true)
+                .username("mn1")
+                .password("1")
+                .fullName("Manager 1")
+                .imageUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_yO9I79sxbYGB2sIGHayqoZ_uzi9mcDh65g&usqp=CAU")
+                .provider(hondaTC)
+                .gender(true)
+                .role(manager)
                 .build());
 
         Vehicle vehicle_1 = vehicleRepository.save(Vehicle.builder()
